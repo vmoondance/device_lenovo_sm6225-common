9edc85d235c8c4d178a589e5f4f1e1eaf326cd0c
Alexander GriefNorth Postol <grief.north@gmail.com>
2024-01-08 23:05:27 +0300

    Revert "Reapply "sm6225-common: Setup for dolby""
    This reverts commit ce7687fa29624444e22e72ef328e5ed3e70e3e04.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -228,5 +228,3 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit the proprietary files
 include vendor/lenovo/sm6225-common/BoardConfigVendor.mk
-# Dolby, if exists
--include vendor/motorola/dolby/BoardConfigDolby.mk
