f59440647bab1d05fd3ba90bd251ea0c969a6869
Alexander GriefNorth Postol <grief.north@gmail.com>
2024-01-09 20:16:42 +0300

    sm6225: bringup SuperiorOS

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,7 +65,7 @@ BOARD_HAVE_QCOM_FM := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/evolution/config/device_framework_matrix.xml
+    vendor/superior/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += \
     $(COMMON_PATH)/manifest.xml \
     $(COMMON_PATH)/manifest_lineage.xml
